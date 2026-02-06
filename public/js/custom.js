9406a7cba61aa81b64a2c5d5ef8116995bbb6a20
wensley <4245450+wensley@users.noreply.github.com>
2026-02-06 18:50:03 +0800

    更新 custom.js
    增加对联

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,9 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+<script>
+  window.cyberCoupletConfig = {
+    leftText: "内卷卷不动就躺",
+    rightText: "摆烂烂出新境界",
+    topText: "躺平真爽"
+  };
+</script>
+<script src="https://cdn.jsdelivr.net/gh/dongfg/cyber-couplet@master/couplet.js"></script>
